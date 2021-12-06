2e8baa0447d5cd37a2b2f4656468b5e1aaae4419
owororo <ter_r_rarium@mail.ru>
2021-12-06 22:15:58 +0300

    blog_1

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
 # Create your views here.
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog import views
 
 urlpatterns = [
+    path('', views.post_list, name='post_list'),
     path('admin/', admin.site.urls),
 ]
